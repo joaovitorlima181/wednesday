874154bf0c57e3636a3420652c01655cccc10e7f
Ana Flávia <anaflaviad805@gmail.com>
2020-10-07 12:48:50 -0300

    editei login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,6 +10,7 @@
 
     <form method="post">
         @csrf
+        
         <div class="form-group">
             <label for="email">E-mail</label>
     <input type="email" name="email" id="email" required class="form-control">
